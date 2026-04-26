e3cabbc33aff54daab35313bf239372601badce5
lapointe <gabriel.lapointe@eurecom.fr>
2026-04-26 12:42:03 +0200

    changement du main afin de trouver app

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ NeuroBeat — Entry point.
 Run from the project root:  python main.py
 """
 
-from app import App
+# On importe la classe App depuis le dossier src
+from src.app import App 
 
 if __name__ == "__main__":
     App().run()
